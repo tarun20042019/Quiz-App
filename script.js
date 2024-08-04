d370cf7454fdab9ae98556b8ebe986118ce21444
tarun20042019 <tarunbalak1312@gmail.com>
2024-08-04 22:38:35 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
+let q=[
+    {
+        numbe:1,
+        question:"What does HTML stand for?",
+        answer:"D) Hyper Text Markup Language",
+        options:[
+            "A) Home Tool Markup Language",
+            "B) Hyperlinks and Text Markup Language",
+            "C) Hyperlinking Text Mark Language",
+            "D) Hyper Text Markup Language"
+
+        ]
+    },
+    {
+        numbe:2,
+        question:"What does MERN stand for?",
+        answer:"A) MongoDB, Express.js, React, Node.js",
+        options:[
+            "A) MongoDB, Express.js, React, Node.js",
+            "B) MySQL, Express.js, React, Node.js",
+            "C) MongoDB, Ember.js, React, Node.js",
+            "D) MongoDB, Express.js, Redux, Node.js"
+
+        ]
+
+
+    },
+
+    {
+        numbe:3,
+        question:"Which property is used to change the background color in CSS?",
+        answer:"C) background-color",
+        options:[
+            "A) bg-color",
+            "B) color",
+            "C) background-color",
+            "D) bgcolor"
+
+        ]
+    },
+    {
+        numbe:4,
+        question:"What does CSS stand for?",
+        answer:"C) Cascading Style Sheets",
+        options:[
+            "A) Computer Style Sheets",
+            "B) Creative Style Sheets",
+            "C) Cascading Style Sheets",
+            "D) Colorful Style Sheets"
+
+        ]
+
+    },
+
+    {
+        numbe:5,
+        question:" Which attribute is used to specify that an input field must be filled out before submitting the form?",
+        answer:"C) required",
+        options:[
+            "A) placeholder",
+            "B) validate",
+            "C) required",
+            "D) formvalidate"
+
+        ]
+
+    }
+
+   
+]
+
 const startbutton=document.querySelector('.startbut');
 const rulepage=document.querySelector('.rulespage');
 const exitbut=document.querySelector('.exitbutton');
